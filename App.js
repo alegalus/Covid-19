968c17bcd67e01c54bd67c40bc87d2667fe460ac
alegalus <alejandrogalus83@yahoo.com.ar>
2021-01-28 12:16:28 -0300

    primer commit, con archivos html, CSS y js

---
+++ App.js
@@ -0,0 +1,35 @@
+window.onload = iniciar
+
+function iniciar(){
+    let button = document.getElementById("cargarEstadistica")
+    button.addEventListener("click", clickBoton)
+
+}
+
+async function cargarUrl(url){
+    let response = await fetch(url)
+    return response.json()
+
+}
+
+async function clickBoton(){
+
+    let pais = document.getElementById("selectPais").value
+    let fecha = document.getElementById("inputFecha").value
+    
+
+    let json = await cargarUrl(`https://api.covid19tracking.narrativa.com/api/${fecha}/country/${pais}`)
+    console.log(json)
+    let estadisticas = json.dates[fecha].countries[pais]
+    console.log(estadisticas)
+    document.getElementById("today_confirmed").innerHTML = estadisticas.today_confirmed
+    document.getElementById("today_deaths").innerHTML = estadisticas.today_deaths
+    document.getElementById("today_new_confirmed").innerHTML = estadisticas.today_new_confirmed
+    document.getElementById("today_new_deaths").innerHTML = estadisticas.today_new_deaths
+    document.getElementById("today_new_recovered").innerHTML = estadisticas.today_new_recovered
+    document.getElementById("today_open_cases").innerHTML = estadisticas.today_open_cases
+    document.getElementById("today_recovered").innerHTML = estadisticas.today_recovered
+    
+}
+
+
