e1cc96de38e395d71c97ac8f4e3f1b66098efb6d
alegalus <alejandrogalus83@yahoo.com.ar>
2021-06-14 20:24:24 -0300

    Modificaciones varias

--- App.js
+++ App.js
@@ -1,54 +1,46 @@
-window.onload = iniciar
+window.onload = iniciar;
 
-function iniciar(){
-    let button = document.getElementById("cargarEstadistica")
-    button.addEventListener("click", clickBoton)
-   
-
-
-
+function iniciar() {
+  let button = document.getElementById("cargarEstadistica");
+  button.addEventListener("click", clickBoton);
 }
 
-async function cargarUrl(url){
-    let response = await fetch(url)
-    return response.json()
-
+async function cargarUrl(url) {
+  let response = await fetch(url);
+  return response.json();
 }
 
+async function clickBoton() {
+  let spinner = document.getElementById("donutSpinner");
+  spinner.style.display = "block";
 
-async function clickBoton(){
-    let spinner = document.getElementById("donutSpinner")
-    spinner.style.display = "block"
+  let modal1 = document.getElementById("modal");
+  modal1.style.display = "block";
 
-    let modal1 = document.getElementById("modal")
-    modal1.style.display = "block"
+  let pais = document.getElementById("selectPais").value;
+  let fecha = document.getElementById("inputFecha").value;
 
-    let pais = document.getElementById("selectPais").value
-    let fecha = document.getElementById("inputFecha").value
-    
+  let json = await cargarUrl(
+    `https://api.covid19tracking.narrativa.com/api/${fecha}/country/${pais}`
+  );
+  console.log(json);
+  let estadisticas = json.dates[fecha].countries[pais];
+  console.log(estadisticas);
+  document.getElementById("today_confirmed").innerHTML =
+    estadisticas.today_confirmed.toLocaleString();
+  document.getElementById("today_deaths").innerHTML =
+    estadisticas.today_deaths.toLocaleString();
+  document.getElementById("today_new_confirmed").innerHTML =
+    estadisticas.today_new_confirmed.toLocaleString();
+  document.getElementById("today_new_deaths").innerHTML =
+    estadisticas.today_new_deaths.toLocaleString();
+  document.getElementById("today_new_recovered").innerHTML =
+    estadisticas.today_new_recovered.toLocaleString();
+  document.getElementById("today_open_cases").innerHTML =
+    estadisticas.today_open_cases.toLocaleString();
+  document.getElementById("today_recovered").innerHTML =
+    estadisticas.today_recovered.toLocaleString();
 
-    let json = await cargarUrl(`https://api.covid19tracking.narrativa.com/api/${fecha}/country/${pais}`)
-    console.log(json)
-    let estadisticas = json.dates[fecha].countries[pais]
-    console.log(estadisticas)
-    document.getElementById("today_confirmed").innerHTML = estadisticas.today_confirmed.toLocaleString()
-    document.getElementById("today_deaths").innerHTML = estadisticas.today_deaths.toLocaleString()
-    document.getElementById("today_new_confirmed").innerHTML = estadisticas.today_new_confirmed.toLocaleString()
-    document.getElementById("today_new_deaths").innerHTML = estadisticas.today_new_deaths.toLocaleString()
-    document.getElementById("today_new_recovered").innerHTML = estadisticas.today_new_recovered.toLocaleString()
-    document.getElementById("today_open_cases").innerHTML = estadisticas.today_open_cases.toLocaleString()
-    document.getElementById("today_recovered").innerHTML = estadisticas.today_recovered.toLocaleString()
-    
-    
-    spinner.style.display = "none"
-    modal1.style.display = "none"
-  
-    
-    
-
+  spinner.style.display = "none";
+  modal1.style.display = "none";
 }
-
-
-
-
-
